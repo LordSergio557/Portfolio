230da3c9f89faab3ede3ac2f312961a89cc2293a
SergioGarcía <sergioporterodefinsove@gmail.com>
2022-02-16 17:09:17 +0100

    actualización de estado

---
+++ public_html/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once("./inc/functions.inc");
+  $mysqli = connectaBBDD("localhost", "id17678852_sergio", "Daw22022SGR.", "id17678852_portafoliodb");
+
+  
+
+  if (!$mysqli) {
+    echo "Error: Conexió amb BBDD";
+    exit();
+  }
+ ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login to LordSergio</title>
+  </head>
+  <body>
+    <form action="index.php" method="post">
+
+    </form>
+  </body>
+</html>
